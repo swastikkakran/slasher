ddefcef8645abe2141add52494e5373120b7544a
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-02 10:02:08 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true,
+        required: true
+    },
+    passowrd: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+
+//pre hooks
+userSchema.pre("save", async function () {
+    if (!this.isModified("passowrd")) return;
+
+    this.passowrd = bcrypt.hash(this.passowrd, 10)
+})
+
+
+//schema methods
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.passowrd)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id
+    }, process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
+}
+
+export const userModel = mongoose.model("User", userSchema)
