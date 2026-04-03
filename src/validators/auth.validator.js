4ddd24a669c8c624e05118459e8a8f9ccb38ef1d
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-03 13:48:32 +0530

    add registerUser validation schema

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const registerSchema = z.object({
+    username: z
+        .string()
+        .min(3, "Username must be at least 3 characters")
+        .max(20, "Username must be at most 20 characters")
+        .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"),
+    email: z
+        .string()
+        .email("Invalid email format"),
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters")
+        .regex(/^(?=.*[A-Z])(?=.*\d).+$/, "Password must contain at least 1 uppercase letter and 1 number")
+});
+
+export { registerSchema }
