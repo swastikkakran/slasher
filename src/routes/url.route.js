cacd1b0fa8dba58e8b586eaae518b78b2ba80243
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-06 23:01:31 +0530

    add api/v1/urls/:shortCode in routes

--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { shortenUrl, getUrls } from "../controllers/url.controller.js";
+import { shortenUrl, getUrls, getSingleUrl } from "../controllers/url.controller.js";
 import { verify_access_JWT } from "../middlewares/auth.middleware.js";
 import { shortenUrlSchema } from "../validators/url.validator.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
-router.route("/shorten").post(verify_access_JWT, validateRequest(shortenUrlSchema), shortenUrl)
 router.route("/").get(verify_access_JWT, getUrls)
+router.route("/shorten").post(verify_access_JWT, validateRequest(shortenUrlSchema), shortenUrl)
+router.route("/:shortCode").get(verify_access_JWT, getSingleUrl)
 
 export default router
