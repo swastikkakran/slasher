382a2ff08b52d4c9987e1f86eca9c99726bdb956
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-06 23:27:44 +0530

    add delete url route

--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortenUrl, getUrls, getSingleUrl, updateUrl } from "../controllers/url.controller.js";
+import { shortenUrl, getUrls, getSingleUrl, updateUrl, deleteUrl } from "../controllers/url.controller.js";
 import { verify_access_JWT } from "../middlewares/auth.middleware.js";
 import { shortenUrlSchema } from "../validators/url.validator.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
@@ -10,5 +10,6 @@ router.route("/").get(verify_access_JWT, getUrls)
 router.route("/shorten").post(verify_access_JWT, validateRequest(shortenUrlSchema), shortenUrl)
 router.route("/:shortCode").get(verify_access_JWT, getSingleUrl)
 router.route("/:shortCode").patch(verify_access_JWT, updateUrl)
+router.route("/:shortCode").delete(verify_access_JWT, deleteUrl)
 
 export default router
