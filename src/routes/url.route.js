992c469d2ea1b8cd52248f3e0abc5745829bb300
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-06 23:17:59 +0530

    add update url route

--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortenUrl, getUrls, getSingleUrl } from "../controllers/url.controller.js";
+import { shortenUrl, getUrls, getSingleUrl, updateUrl } from "../controllers/url.controller.js";
 import { verify_access_JWT } from "../middlewares/auth.middleware.js";
 import { shortenUrlSchema } from "../validators/url.validator.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
@@ -9,5 +9,6 @@ const router = Router();
 router.route("/").get(verify_access_JWT, getUrls)
 router.route("/shorten").post(verify_access_JWT, validateRequest(shortenUrlSchema), shortenUrl)
 router.route("/:shortCode").get(verify_access_JWT, getSingleUrl)
+router.route("/:shortCode").patch(verify_access_JWT, updateUrl)
 
 export default router
