94ee4277490f7310133d1a94d63e55fbbdfd2f2e
Swastik Kakran <kakranswastik2007@gmail.com>
2026-04-06 22:47:25 +0530

    add /api/v1/urls to routes

--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/url.controller.js";
+import { shortenUrl, getUrls } from "../controllers/url.controller.js";
 import { verify_access_JWT } from "../middlewares/auth.middleware.js";
 import { shortenUrlSchema } from "../validators/url.validator.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
@@ -7,5 +7,6 @@ import { validateRequest } from "../middlewares/validate.middleware.js";
 const router = Router();
 
 router.route("/shorten").post(verify_access_JWT, validateRequest(shortenUrlSchema), shortenUrl)
+router.route("/").get(verify_access_JWT, getUrls)
 
 export default router
